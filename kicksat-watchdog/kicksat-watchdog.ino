a604a7f03e0e94665a23733500d61a4253f23f78
curransinha <curransinha@gmail.com>
2016-04-04 13:09:16 -0400

    New watchdog code...basic scheduler added in mainboard.ino

--- kicksat-watchdog/kicksat-watchdog.ino
+++ kicksat-watchdog/kicksat-watchdog.ino
@@ -1,13 +1,18 @@
 #include <EasyTransfer.h>
+#include <LowPower.h>
+#include <avr/wdt.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
 
 // Pins and Constants
 #define RESET_ENB   12
 #define LED         13
-#define WATCHDOG_TIMEOUT 5000
+#define WD_PIN      0
 
 unsigned long last_updated = 0;  // seconds
 unsigned long now = 0;    // milliseconds
-char reset_flag = 0;     // for hysteresis
+volatile char reset_flag = 0;     // for hysteresis
+int led_flip = 1;
 
 // Structs for serial communication
 
@@ -16,64 +21,111 @@ typedef struct info {
   double x_ecef;
   double y_ecef;
   double z_ecef;
+  int32_t x_vel;
+  int32_t y_vel;
+  int32_t z_vel;
   uint8_t burnwires;
 } info;
-
 
 EasyTransfer ET_data;
 
 info sat_info;
 
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600);
+//  Serial.begin(9600);
   
   pinMode(RESET_ENB, OUTPUT);
   pinMode(LED, OUTPUT);
+  pinMode(WD_PIN, INPUT);
   
-  ET_data.begin(details(sat_info), &Serial1);
+//  ET_data.begin(details(sat_info), &Serial);
 
-  delay(1000);
-  digitalWrite(LED, HIGH);
-  Serial.println("Watchdog Ready");
+//  delay(1000);
+  for (int k = 0; k < 3; k++) {
+    digitalWrite(LED, HIGH);
+    delay(250);
+    digitalWrite(LED, LOW);
+    delay(250);
+   }
+  digitalWrite(LED, LOW);
+//  wdt_enable(WDTO_8S);
+//  wdt_setup();
+  pinSetup();
+//  Serial.println("Watchdog Ready");
+
 }
 
 void loop() {
-  
-  now = millis();
-
-  if (ET_data.receiveData()) {
-    // new gps data received
-    Serial.print("seconds: ");
-    Serial.printf("%llu\n",sat_info.seconds);
-    Serial.print("x: ");
-    Serial.println(sat_info.x_ecef);
-    Serial.print("burnwires: ");
-    Serial.println(sat_info.burnwires);
-    digitalWrite(LED, LOW);
-    // Write new data to EEPROM
-    // send ack??
-    last_updated = now;
-  } 
-//  if (ET_beat.receiveData()) {
-//    Serial.println("heartbeat received");
-//    last_updated = now;
-//    // update EEPROM with last updated
-//    // send an ack??
-//  }
-
-  if ((now - last_updated) > WATCHDOG_TIMEOUT) {
-    // write time of reset to EEPROM??
-    Serial.println("watchdog reset triggered");
-    resetBoard();
-    digitalWrite(LED, HIGH);
-  }
-
-  // sleep??
-//  delay (2000);
-    
+  LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
+//  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+//  sleep_enable();
 }
 
+void wdt_setup() {
+
+  wdt_reset();
+  // disable interrupts
+  cli();
+  // Enter Watchdog Configuration mode:
+  WDTCSR |= (1<<WDCE) | (1<<WDE);   
+  // Set Watchdog settings:
+  //        enb int     no reset
+  // WDTCSR = (1<<WDIE) | (0<<WDE) ;
+   WDTCSR = 0b01100000;   // last bit goes between 4 and 8 secs
+   sei();
+}
+
+ISR(WDT_vect) {
+  digitalWrite(LED, HIGH);
+//  (led_flip) ? digitalWrite(LED, HIGH) : digitalWrite(LED, LOW);
+//  led_flip = !led_flip;
+//  if (reset_flag) {
+//    digitalWrite(LED,HIGH);
+//  }
+  reset_flag = 1;
+}
+
+ISR(PCINT2_vect) {
+//  if (digitalRead(WD_PIN)) {
+  if(PIND & (1 << PD1)){
+    reset_flag = 0;
+    digitalWrite(LED, LOW);
+    wdt_reset();
+//    (led_flip) ? digitalWrite(LED, HIGH) : digitalWrite(LED, LOW);
+//    led_flip = !led_flip;
+  }
+}
+
+void sleepNow() {
+
+    attachInterrupt(0, pinInterrupt, LOW);
+    delay(50);
+    // Choose our preferred sleep mode:
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+ 
+    // Set sleep enable (SE) bit:
+    sleep_enable();
+ 
+    // Put the device to sleep:
+    sleep_mode();
+ 
+    // Upon waking up, sketch continues from this point.
+    sleep_disable();
+}
+
+void pinSetup() {
+  cli();
+  PCICR |= 0b00000100;    // turn on port d
+  PCMSK2 |= 0b00000010;    // ,PCINT17, physical pin tx0
+  sei();
+}
+
+void pinInterrupt(void)
+{
+    detachInterrupt(0);
+}
+
+// TODO: fix this
 void resetBoard() {
   if (reset_flag == 0) {
     reset_flag = 1;
@@ -87,9 +139,4 @@ void resetBoard() {
     delay(100);
   }
   
-  
-//  digitalWrite(RESET_ENB, HIGH);
-//  delay(4000);
-//  digitalWrite(RESET_ENB, LOW);
-  // recovery from reset process?
-}
+
