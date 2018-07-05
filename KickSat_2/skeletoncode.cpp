3dc0c777ab0568df1214fb92abd09cddc79a5eb3
Connor Settle <connor.settle@gmail.com>
2018-07-05 10:56:42 -0700

    updated volatiles

--- KickSat_2/skeletoncode.cpp
+++ KickSat_2/skeletoncode.cpp
@@ -15,7 +15,11 @@ int deploy_sprites = 0; //deployment flag, indicates when we are ready to deploy
 int antenna_deployed = 0; //holds antenna status
 int high_chirp_freq = 0; //indicates whether we are using high or low frequency chirping
 int chirp_signal = 0; //flag that goes high when its time to chirp
+
+volatile int current_tick = 0; //global counter that increments every tick
 volatile int time_before_next_chirp = 0; //decrements every second in WDT loop
+volatile int time_before_next_sensor_data = 20; //TODO: choose good initial value
+
 bool high_frequency_mode = false;
 int num_chirps_until_switch = 0;
 
@@ -23,13 +27,17 @@ int num_chirps_until_switch = 0;
 #define no_uplink_num_chirps 100
 #define high_freq_num_chirps 300
 
+//pin definitions
+#define WDT_WDI 12
+#define LED0 9
+
 
 int main(void) {
 	//once power is on, we begin our initialization sequence
-	//TODO: add all init stuff, handle config
 	init_timers();
 	handle_config();
-	//TODO: check if the antenna is down, if so, go to initial_mode
+	
+	//check if the antenna is down, if so, go to initial_mode
 	if(!antenna_deployed) {
 		initial_mode(); //fix timers here
 	}
@@ -39,6 +47,7 @@ int main(void) {
 	//time to deploy
 	deployment();
 	while(true) { //go back to chirp mode after deploying sprites
+		//could potentially just forever attempt to deploy, to ensure maximum likelihood of all burn wires working
 		chirp_mode();
 	}
 }
@@ -48,7 +57,10 @@ void handle_config() {
 }
 
 void initial_mode() {
-	//TODO:wait for 15 min, then deploy antenna
+	//wait for 15 min, then extend antenna
+	while(current_tick < 15) {}
+	
+	//extend antenna here
 }
 
 void init_timers() {
@@ -62,17 +74,17 @@ void init_timers() {
 void chirp_mode() {
 
 	while(time_before_next_chirp > 0) {
-		//wait for timer to go off before chirping 
+		//wait for timer to go off before chirping
 		//TODO: uplink handling
 
-		//if (time_before_next_sensor_data <=0) {
+		if (time_before_next_sensor_data <= 0) { //could be decremented in this loop or in WDT loop
 			//gather_sensor_data();
-		//}
+		}
 
 		//if (uplink detected) {
-			//handle_uplink_data()
-			high_frequency_mode = false;
-			num_chirps_until_switch = uplink_num_chirps;
+		//handle_uplink_data()
+		high_frequency_mode = false;
+		num_chirps_until_switch = uplink_num_chirps;
 		//}
 	}
 
@@ -94,18 +106,18 @@ void chirp_mode() {
 	//reset volatile counting integer
 	if (high_frequency_mode) {
 		time_before_next_chirp = 60; //1 minute until next chirp
-	} else {
+		} else {
 		time_before_next_chirp = 270; //4.5 minutes
 	}
 }
 
 void chirp() {
 	//TODO: collect self check data
-		// check battery status
-		// check solar panel status
-		// check current time
+	// check battery status
+	// check solar panel status
+	// check current time
 	//TODO: write to self check data file
-	//TODO: check for file corruption	 
+	//TODO: check for file corruption
 }
 
 
@@ -114,4 +126,29 @@ void deployment() {
 }
 
 
-//Interrupts and Handlers 
+//Interrupts and Handlers
+
+void TC3_Handler() {
+	
+	// Overflow interrupt triggered
+	if ( TC3->COUNT16.INTFLAG.bit.OVF == 1 ) {
+		//toggles WDT
+		REG_PORT_OUTTGL0 = WDT_WDI;
+		
+		//increment/decrement any global volatiles here
+		current_tick++;
+		time_before_next_chirp--;
+		time_before_next_sensor_data--;
+		
+		//clears the overflow interrupt flag
+		REG_TC3_INTFLAG = TC_INTFLAG_OVF;
+	}
+	
+	// Error interrupt triggered
+	else if ( TC3->COUNT16.INTFLAG.bit.ERR == 1 ) {
+		//possibly reset the micro if we ever get here
+		
+		//clears the error interrupt flag
+		REG_TC3_INTFLAG = TC_INTFLAG_ERR;
+	}
+}
