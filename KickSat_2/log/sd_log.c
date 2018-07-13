7c0133024e4a28c90aa1c08db9f1b48a81a5583b
candre97 <candre@usc.edu>
2018-07-13 14:43:09 -0700

    Update sd_log.c

--- KickSat_2/log/sd_log.c
+++ KickSat_2/log/sd_log.c
@@ -18,42 +18,67 @@ String[] sd_log :: data_dump(int numEntries){
   
 }
 
-void sd_log::set_antenna_flag(bool flag){
-
+void sd_log::set_antenna_flag(bool flag)
+{
+  antenna_flag = flag;
+  
   
 }
-void sd_log::set_bw1_flag(bool flag){
-
-  
+void sd_log::set_bw1_flag(bool flag)
+{
+  bw1_flag = flag;
 }
 
-void sd_log::set_bw2_flag(bool flag){
-
-  
+void sd_log::set_bw2_flag(bool flag)
+{
+  bw2_flag = flag; 
 }
-void sd_log::set_bw3_flag(bool flag){
-
-  
-}
-
-void sd_log::set_early_mode(bool flag){
-
-  
-}
-void sd_log::set_standby_mode(bool flag){
-
-  
-}
-void sd_log::set_deploying_mode(bool flag){
-
-  
+void sd_log::set_bw3_flag(bool flag)
+{
+  bw3_flag = flag;
 }
 
-void sd_log::set_deployed_flag(bool flag){
+void sd_log::set_early_mode(bool flag)
+{
+  mode_isEarly = flag;
+}
+void sd_log::set_standby_mode(bool flag)
+{
+  mode_isStandby = flag;
   
 }
-void sd_log::varChanged_flag(bool flag){
+void sd_log::set_deploying_mode(bool flag)
+{
+  mode_isDeploying = flag;
+}
 
-  
+void sd_log::set_deployed_flag(bool flag)
+{
+  didDeploy_flag = flag;
 }
-  
+void sd_log::varChanged_flag(bool flag)
+{
+  varChanged_flag = flag;
+}
+// takes in a byte of information and counts the number of ones --> returns a bool
+bool byte_to_bool(byte inBool)
+{
+  byte mask = 0b10000000; 
+  short int cnt = 0;
+  for(int i = 0; i < 7; i++)
+  {
+    if((mask && inBool) == 0b10000000)
+    {
+      cnt++;
+    }
+    inBool << inBool 1;
+  }
+  if(cnt > 4)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
