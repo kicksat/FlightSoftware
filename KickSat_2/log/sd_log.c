8772c653f24d0b37349e92bde430762a4078acec
eamorgan997 <35286223+eamorgan997@users.noreply.github.com>
2018-07-13 14:34:23 -0700

    Create sd_log.c

---
+++ KickSat_2/log/sd_log.c
@@ -0,0 +1,59 @@
+//sd_log.c
+
+#include sd_log.h
+
+void sd_log :: write_log(){
+
+
+  
+}
+
+String sd_log :: read_entry(int entryIndex){
+
+
+  
+}
+
+String[] sd_log :: data_dump(int numEntries){
+  
+}
+
+void sd_log::set_antenna_flag(bool flag){
+
+  
+}
+void sd_log::set_bw1_flag(bool flag){
+
+  
+}
+
+void sd_log::set_bw2_flag(bool flag){
+
+  
+}
+void sd_log::set_bw3_flag(bool flag){
+
+  
+}
+
+void sd_log::set_early_mode(bool flag){
+
+  
+}
+void sd_log::set_standby_mode(bool flag){
+
+  
+}
+void sd_log::set_deploying_mode(bool flag){
+
+  
+}
+
+void sd_log::set_deployed_flag(bool flag){
+  
+}
+void sd_log::varChanged_flag(bool flag){
+
+  
+}
+  
