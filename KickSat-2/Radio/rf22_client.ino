884587a643259d13f9d7ed1af58e8d3f0244657a
Max Holliday <maholli@stanford.edu>
2018-07-20 10:08:19 -0700

    WORKING rf22_client
    make sure nIRQ is hooked up as commented in RH_RF22 line

--- KickSat-2/Radio/rf22_client.ino
+++ KickSat-2/Radio/rf22_client.ino
@@ -11,49 +11,55 @@
 
 #include <SPI.h>
 #include <RH_RF22.h>
+#include <RHGenericDriver.h>
 
-// Singleton instance of the radio driver
-RH_RF22 rf22;
+
+RHHardwareSPI spi;
+
+RH_RF22 rf22 (10,2,spi); //chipSelect=10, interruptPin=2
 
 void setup() 
 {
-  SerialUSB.begin(115200);
-  if (!rf22.init())
-    SerialUSB.println("init failed");
+ SerialUSB.begin(115200);
+ SerialUSB.println("workings1");
+ SerialUSB.println("workings2");
+ if (!rf22.init())
+   SerialUSB.println("init failed");
   // Defaults after init are 434.0MHz, 0.05MHz AFC pull-in, modulation FSK_Rb2_4Fd36
+spi.attachInterrupt();
+  // rf22.setTxPower(RH_RF22_RF23BP_TXPOW_1DBM);
 }
-
 void loop()
 {
-  SerialUSB.println("Sending to rf22_server");
-  // Send a message to rf22_server
-  uint8_t data[] = "Hello World!";
-  rf22.send(data, sizeof(data));
-  
+
+ SerialUSB.println("Sending to rf22_server");
+ // Send a message to rf22_server
+ uint8_t data[] = "Hello World!";
+ rf22.send(data, sizeof(data));
+ SerialUSB.println("waiting");
   rf22.waitPacketSent();
-  // Now wait for a reply
-  uint8_t buf[RH_RF22_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
-
-  if (rf22.waitAvailableTimeout(500))
-  { 
-    // Should be a reply message for us now   
-    if (rf22.recv(buf, &len))
-    {
-      SerialUSB.print("got reply: ");
-      SerialUSB.println((char*)buf);
-    }
-    else
-    {
-      SerialUSB.println("recv failed");
-    }
-  }
-  else
-  {
-    SerialUSB.println("No reply, is rf22_server running?");
-    delay(100);
-    SerialUSB.print(rf22.statusRead());
-  }
-  delay(400);
+  SerialUSB.println("clear");
+ // Now wait for a reply
+ uint8_t buf[RH_RF22_MAX_MESSAGE_LEN];
+ uint8_t len = sizeof(buf);
+ if (rf22.waitAvailableTimeout(500))
+ {
+   // Should be a reply message for us now  
+   if (rf22.recv(buf, &len))
+   {
+     SerialUSB.print("got reply: ");
+     SerialUSB.println((char*)buf);
+   }
+   else
+   {
+     SerialUSB.println("recv failed");
+   }
+ }
+ else
+ {
+   SerialUSB.println("No reply, is rf22_server running?");
+   delay(100);
+   SerialUSB.print(rf22.statusRead());
+ }
+ delay(400);
 }
-
