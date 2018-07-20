3ff7da5faff3722a3773b3588ae043bbce3bf944
Connor Settle <csettle@stanford.edu>
2018-07-20 13:45:24 -0700

    Create SD_DataFile.cpp

---
+++ KickSat-2/DataHandling/SD_DataFile.cpp
@@ -0,0 +1,15 @@
+//SD_DataFile.cpp
+
+#include "SD_DataFile.h"
+
+SD_DataFile::SD_DataFile(uint16_t dw) {
+  _dataWidth = dw;
+}
+
+bool SD_DataFile::writeDataEntry(byte* data) {
+  
+}
+
+bool SD_DataFile::readDataEntry() {
+  
+}
