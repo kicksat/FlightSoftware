4e9240154fa92f92bdef1aa627bbc7fa5d664a8f
Connor Settle <csettle@stanford.edu>
2018-08-08 12:35:44 -0700

    fixed comiple errors

--- KickSat-2/libraries/Sensors/KickSat_Sensor.h
+++ KickSat-2/libraries/Sensors/KickSat_Sensor.h
@@ -1,4 +1,4 @@
-
+#include <Arduino.h>
 
 #ifndef KICKSAT_SENSOR_H_
 #define KICKSAT_SENSOR_H_
@@ -27,6 +27,6 @@ class KickSat_Sensor
     byte data_filter_chop_reg = 0x17;
     byte vbias_reg = 0x0;
     byte gain_pga_reg = 0x8;
-}
+};
 
 #endif /* KICKSAT_SENSOR_H_ */
