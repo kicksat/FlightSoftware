7d0fde092b00c88b9265a8f517467b5f14207af1
eamorgan997 <35286223+eamorgan997@users.noreply.github.com>
2018-08-08 11:21:19 -0700

    updated sd

--- KickSat-2/libraries/SD/Examples/sd_spi_test.ino
+++ KickSat-2/libraries/SD/Examples/sd_spi_test.ino
@@ -3,29 +3,27 @@
 #include "SD_DataFile.h"
 #include <SD.h>
 
-int n = 0;
+int n = -1;
 sd_log mySd_log;
 void setup(){
-  SerialUSB.begin(9600);
-  delay(2000);
-  SD.begin(10);
+ SerialUSB.begin(9600);
+ delay(2000);
 
-  pinMode(SPI_CS_SD, OUTPUT);
-  mySd_log.sd_init();
+ mySd_log.sd_init();
 
-  mySd_log.logData = {0, (byte)'a', {(byte)'v', (byte)'v', (byte)'v'}, {3.14159, 5454.54, 1111.1}, {1.1, 1.2, 1.3, 2.1, 2.2, 2.3, 3.1, 3.2, 3.3}, {(byte)'c', (byte)'o', (byte)'m', (byte)'m', (byte)'a', (byte)'n', (byte)'d', (byte)'!'}};
-  mySd_log.write_log(mySd_log.logData);
+//  mySd_log.logData = {0, (byte)'a', {(byte)'v', (byte)'v', (byte)'v'}, {3.14159, 5454.54, 1111.1}, {1.1, 1.2, 1.3, 2.1, 2.2, 2.3, 3.1, 3.2, 3.3}, {(byte)'c', (byte)'o', (byte)'m', (byte)'m', (byte)'a', (byte)'n', (byte)'d', (byte)'!'}};
+//  mySd_log.write_log(mySd_log.logData);
 
 }
 
 void loop(){
- mySd_log.sd_init();
- mySd_log.logData.log_num = n;
- mySd_log.write_log(mySd_log.logData);
- SerialUSB.print("Wrote Log ");
- SerialUSB.println(mySd_log.logData.log_num);
- SerialUSB.println(mySd_log.read_entry(n));
- n++;
- delay(1000);
- mySd_log.sd_end();
+mySd_log.sd_refresh();
+mySd_log.logData.log_num = n;
+mySd_log.write_log(mySd_log.logData);
+SerialUSB.print("Wrote Log ");
+SerialUSB.println(mySd_log.logData.log_num);
+SerialUSB.println(mySd_log.read_entry(n));
+delay(1000);
+mySd_log.sd_end();
+n++;
 }
