ab3b69349936157192e4357040f2aae69e04a369
Connor Settle <csettle@stanford.edu>
2018-08-13 16:25:43 -0700

    Add files via upload

---
+++ KickSat-2/libraries/Checksum/ChecksumHandler.cpp
@@ -0,0 +1,19 @@
+#include "ChecksumHandler.h"
+
+byte ChecksumHandler::calculateChecksum(byte* message, int len) {
+  byte val = 0;
+  for (int i = 0; i < len; i++) {
+    val ^= message[i];
+  }
+  return val;
+}
+
+bool ChecksumHandler::evaluateChecksum(byte* message, int len) {
+  byte val = 0;
+  for (int i = 0; i < len; i++) {
+    val ^= message[i];
+  }
+  return val == 0;
+}
+
+ChecksumHandler Checksum;
---
+++ KickSat-2/libraries/Checksum/ChecksumHandler.h
@@ -0,0 +1,18 @@
+// ChecksumHandler.h - Library for checksums
+
+#ifndef CHECKSUMHANDLER_h
+#define CHECKSUMHANDLER_h
+
+#include <Arduino.h>
+
+class ChecksumHandler {
+  
+  public:
+    byte calculateChecksum(byte* message, int len);
+    bool evaluateChecksum(byte* message, int len);
+    
+};
+
+extern ChecksumHandler Checksum;
+
+#endif
