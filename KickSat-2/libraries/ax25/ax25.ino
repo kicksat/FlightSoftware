91d73225ec815c6906f62d8fdd82a49e360c59fc
Ralen Toledo <10423997+ralent@users.noreply.github.com>
2018-08-14 09:48:08 -0700

    minor formatting fixes

--- KickSat-2/libraries/ax25/ax25.ino
+++ KickSat-2/libraries/ax25/ax25.ino
@@ -1,27 +1,37 @@
 #include "ax25.h"
 
 void setup() {
+
   SerialUSB.begin(115200);
   while(!SerialUSB);
   SerialUSB.println("Serial Initialized");
   delay(500);
+
   char message[256];
   byte* encodedMessage;
   char* decodedMessage;
+
   strcpy(message,"Hello world from the KickSat team!");
   SerialUSB.print("Message:"); SerialUSB.println(message);
+
   SerialUSB.println("Encoding message...");
   encodedMessage = ax25.encodePacket(message);
   SerialUSB.println("Message encoded...");
+
   for (int i = 0; i < 450; i++) {
     SerialUSB.print(encodedMessage[i], BIN);
     SerialUSB.print(" ");
   }
+
+  SerialUSB.println();
+  SerialUSB.println("Decoding message...");
   decodedMessage = ax25.decodePacket(encodedMessage,450);
-  SerialUSB.println("Final decoded Message");
+  SerialUSB.println("Message decoded...");
+
   for (int i=0; i < strlen(decodedMessage); i++) {
     SerialUSB.print(decodedMessage[i]);
   }
+
 }
 
 void loop() {
