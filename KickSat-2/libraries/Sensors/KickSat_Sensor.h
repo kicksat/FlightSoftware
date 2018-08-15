4cf434ab806a39ac77b005d8b865da7dc0b5b3cc
Connor Settle <csettle@stanford.edu>
2018-08-15 13:39:46 -0700

    added checksum validation

--- KickSat-2/libraries/Sensors/KickSat_Sensor.h
+++ KickSat-2/libraries/Sensors/KickSat_Sensor.h
@@ -13,8 +13,9 @@ class KickSat_Sensor
     void operate(byte* dataOut);
     void parseMessage(String msg, String arg[]);
     void handleCommand(String cmd, byte* buf, int* index);
-	String getCommand(String data, char separator, int index);
-	void rewriteConfig(byte* buf, int len);
+    String getCommand(String data, char separator, int index);
+    bool validateConfigFiles();
+    void rewriteConfig(byte* buf, int len);
     void burstWriteRegs(byte* data, uint8_t len);
     void startADC();
     void stopADC();
@@ -28,7 +29,7 @@ class KickSat_Sensor
     int _ADCreset;
     int _SDchipSelect;
     String _configFileName;
-	File _configFile;
+    File _configFile;
 
     byte data_filter_chop_reg = 0x17;
     byte vbias_reg = 0x0;
