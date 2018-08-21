3e51e6799a517b2fa49f6ba2e42fb92e8c24bc09
eamorgan997 <35286223+eamorgan997@users.noreply.github.com>
2018-08-21 11:07:23 -0700

    uplink working!

--- KickSat-2/libraries/uplink/uplink.h
+++ KickSat-2/libraries/uplink/uplink.h
@@ -10,98 +10,99 @@ Purpose: Library for handling the reading and processing of uplink to the satell
 
 #include <KickSatLog.h>
 #include <ChecksumHandler.h>
+#include <KickSatConfig.h>
+#include <RTCCounter.h>
 // #include <ax25.h>
 // #include <RadioHead.h>
-#define NUM_COMMANDS //redacted
-#define COMMAND_WIDTH //redacted
 
 
-ChecksumHandler checksumHandler;
-bool armingMode;
+#define LISTENINGDURATION 1500 // milliseconds
 
-const byte commandDict[NUM_COMMANDS][COMMAND_WIDTH] = {/* REDACTED */};
+#define NUM_COMMANDS 4//redacted
+#define NUM_COMMANDS_ARM 1
+#define COMMAND_WIDTH 3//redacted
+#define UINT8_FALSE 255
+#define METADATA_WIDTH 255
 
-bool parseUplink(char *buf);
-void processUplink(char *buf);
+const byte commandDict[NUM_COMMANDS][COMMAND_WIDTH] = {{'d','o','g'},{'c','a','t'},{'p','i','g'},{'o','w','l'}/* REDACTED */};
+const byte commandDictArm[NUM_COMMANDS_ARM][COMMAND_WIDTH] = {{'b','r','n'}};
+
+
+uint8_t parseUplink(char* buf);
+uint8_t extractArmedCommand(byte* buf); 
+uint8_t extractCommand(byte* buf); 
+int getSensorMetadata(char* buf, char* metadata);
+
 
 ///////////////////////
 // Listen for uplink //
-///////////////////////
-bool listenForUplink(char *buf, uint8_t duration) {
-  timeout.start(duration); // Start timeout timer for the listening duration
+//////////////////////////////////////////////////////////////////////////////
+uint8_t listenForUplink(char *buf, uint8_t duration){
+  //SerialUSB.println("Listening for uplink...");
+  timeout.start(duration);
   while(1) { // Wait for uplink, retreive from buffer
-    // if (radio.available()) { // TODO: change if radio is available, not serial
-    if (SerialUSB.available()) { // If data is in buffer to be read
-      if (parseUplink(buf)) { // Read from buffer and validate uplink
-        return true;
+    if (SerialUSB.available()) { // TODO: change if radio is available, not serial
+      uint8_t command = parseUplink(buf);
+      if (command != UINT8_FALSE) {
+        return command;
       }
     }
     if (timeout.triggered()) { // Checks time for timeout
       SerialUSB.println("Uplink Timeout");
-      return false;
+      return UINT8_FALSE;
     }
   }
 }
 
-/////////////////////////////////////
-// Listen for uplink in armingmode //
-/////////////////////////////////////
-bool listenForUplinkArmingMode(char *buf, uint8_t duration) {
-  timeout.start(duration); // Start timeout timer for the listening duration
-  while(1) { // Wait for uplink, retreive from buffer
-    // if (radio.available()) { // TODO: change if radio is available, not serial
-    if (SerialUSB.available()) { // If data is in buffer to be read
-      if (parseUplink(buf)) { // Read from buffer and validate uplink
-        processUplink(); // Process uplink while in arming mode
-      }
-    }
-    if (timeout.triggered()) { // Checks time for timeout
-      SerialUSB.println("Uplink Timeout");
-      break;
-    }
-    if (getDB1status() && getDB2status() && getDB3status()) {
-      return true;
-    }
-  }
-  if (getDB1status() || getDB2status() || getDB3status()) {
-    return true;
-  }
-}
 
-//////////////////
-// Parse uplink //
-//////////////////
-bool parseUplink(char *buf) {
-  // Read available data from buffer
-  // TODO: change from Serial.read to radio.read(char *buf)
+uint8_t parseUplink(char* buf){
+  ////////////////////////////////////////////////////////
   uint32_t i = 0;
-  // while(radio.available() > 0) { // Read until the entire buffer has been read // TODO: This function doesn't exist but should
-  //   buf[i] = radio.read(); // Reads the available char (ASCII) // TODO: This function doesn't exist but should
+  SerialUSB.println("read from buffer: ");
   while(SerialUSB.available() > 0) { // Read until the entire buffer has been read
     buf[i] = SerialUSB.read(); // Reads the available char (ASCII)
+    SerialUSB.print(buf[i]);
     i++;
   }
+
+  buf[i] = Checksum.calculateChecksum((byte*)buf, i);/////Added in so that the checksum works, usually will be included in uplink
+  i++;
   buf[i] = '\0';
+  SerialUSB.println();
 
   // Checksum data
-  // if (checksum(buf)) // Verifies checksum of read uplink // TODO: Incorporate real checksum check here for input
-  if (checksumHandler.evaluateChecksum(buf, i)) { // If uplink is valid, to be replaced by checksum validation
+  if (Checksum.evaluateChecksum((byte*)buf, i)) { // If uplink is valid, to be replaced by checksum validation
     SerialUSB.print("ACK:"); // If checksum is valid, respond with ACK
     SerialUSB.println(buf);
-    // radio.send("ACK") // If checksum is valid, respond with ACK // TODO: This function doesn't exist but should
-    return true; // Return successfull uplink
-  } else {
-    SerialUSB.print("NACK:");
-    SerialUSB.println(buf);
-    // radio.send("NACK") // If checksum is not valid, respond with NACK // TODO: This function doesn't exist but should
-    return false; // Return unsuccessfull uplink
-  }
+    // radio.send("ACK") // If checksum is valid, respond with ACK // TODO: This function doesn't exist but should    
+    uint8_t command;
+    if(configFile.getArmedStatus()){
+      command = extractArmedCommand((byte*)buf);
+    }else{
+      command = extractCommand((byte*)buf); // returns index of command if found, UINT8_FALSE if no command exists
+    }
+    //SerialUSB.println(command);
+    if(command != UINT8_FALSE){
+      SerialUSB.print("Command: ");
+      for(int f = 0; f < COMMAND_WIDTH; f++){
+        SerialUSB.print((char)commandDict[command][f]);
+      } 
+      SerialUSB.println();
+
+      return command;
+    }
+  } 
+  SerialUSB.print("NACK:");
+  SerialUSB.println(buf);
+  // radio.send("NACK") // If checksum is not valid, respond with NACK // TODO: This function doesn't exist but should
+  return UINT8_FALSE; // Return unsuccessfull uplink 
+  ////////////////////////////////////////////////////////
 }
 
 ///////////////////
 // Parse command //
 ///////////////////
-uint8_t extractCommand(byte* buf) {
+uint8_t extractCommand(byte* buf) { 
   for (int i = 0; i < NUM_COMMANDS; i++) {
     bool flag = true;
     for (int n = 0; n < COMMAND_WIDTH; n++) {
@@ -114,59 +115,99 @@ uint8_t extractCommand(byte* buf) {
       return i;
     }
   }
+  SerialUSB.println("INVALID COMMAND");
+  return UINT8_FALSE;
 }
+
+uint8_t extractArmedCommand(byte* buf) { 
+  for (int i = 0; i < NUM_COMMANDS_ARM; i++) {
+    bool flag = true;
+    for (int n = 0; n < COMMAND_WIDTH; n++) {
+      if (commandDictArm[i][n] != buf[n]) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      return i + NUM_COMMANDS;
+    }
+  }
+  SerialUSB.println("INVALID COMMAND");
+  return UINT8_FALSE;
+}
+
 
 ////////////////////
 // Process uplink //
 ////////////////////
-void processUplink(char *buf) {
-
-  uint8_t command = extractCommand(buf);
+void processUplink(char* buf, uint8_t command){
   switch(command) { // respond to the command
-    case 0: // Burn wires
-    SerialUSB.println("Command: Burn Wire");
-    byte metadata = buf[COMMAND_WIDTH]; //grab the first byte after the command, this is the matadata byte
-    if (metadata == 0x01) { // Burn wire one
-      setDB1Deployed();
-    } else if (metadata == 0x02) { // Burn wire two
-      setDB2Deployed();
-    } else if (metadata == 0x03) { // Burn wire three
-      setDB3Deployed();
-    } else if (metadata == 0x0F) { // Burn all wires
-      setDB1Deployed();
-      setDB2Deployed();
-      setDB3Deployed();
-    }
-    break;
+    case 0: // Downlink
+      SerialUSB.println("Command: Start Downlink");
+      //TODO: increase the frequency of beacons and begin sending older log data
+      //NOTE: this is its own function, which shits out a bunch of data
+      break;
 
-    case 1: // Begin downlink
-    SerialUSB.println("Command: Start Downlink");
-    //TODO: increase the frequency of beacons and begin sending older log data
-    //NOTE: this is its own function, which shits out a bunch of data
-    break;
+    case 1: // set Arming mode
+      SerialUSB.println("Command: Enter Arming Mode");
+      configFile.setArmed(); // Set flag to enter arming mode
+      break;
+      
+    case 2:{// reflash sensor config
+      char metadata[METADATA_WIDTH];
+      int metadataLen = getSensorMetadata(buf, metadata);
+      SerialUSB.println("Command: Uplink Sensor Config");
+      
+      //TODO: read data from uplink and write new data to sensor config files
+      //IMPORTANT: any uplink can be no longer than 64 bytes so configs must be short
+     }
+      break;
+    
+    case 3: // No command --> go back to sleep and go through another standby mode loop
+      SerialUSB.println("KickSat is alive! going back to sleep");
+      // radio.send(ax25("KickSat is alive!")); // Send health data through radio // TODO: This function doesn't exist yet but should and should be syntactically incorrect
+      break;      
+      
+    case 4: {//burn wires
+       SerialUSB.println("Command: Burn Wire");
+       byte metadata = buf[COMMAND_WIDTH]; //grab the first byte after the command, this is the burn metadata byte
+       if (metadata == (byte)'1') { // Burn wire one
+        configFile.setDB1Flag();
+       } else if (metadata == (byte)'2') { // Burn wire two
+        configFile.setDB2Flag();
+       } else if (metadata == (byte)'3') { // Burn wire three
+        configFile.setDB3Flag();
+       } else if (metadata == (byte)'a') { // Burn all wires
+        configFile.setDB1Flag();
+        configFile.setDB2Flag();
+        configFile.setDB3Flag();
+      }
+     }
+     break;
 
-    case 2:
-    SerialUSB.println("Command: Uplink Sensor Config");
-    //TODO: read data from uplink and write new data to sensor config files
-    //IMPORTANT: any uplink can be no longer than 64 bytes so configs must be short
-    break;
-
-    case 3: // Enter arming mode, exit standby mode
-    SerialUSB.println("Command: Enter Arming Mode");
-      setArmed(); // Set flag to enter arming mode
-    break;
-
-
-    case 4: // No command --> go back to sleep and go through another standby mode loop
-    SerialUSB.println("KickSat is alive!");
-    // radio.send(ax25("KickSat is alive!")); // Send health data through radio // TODO: This function doesn't exist yet but should and should be syntactically incorrect
-    break;
 
     default: // No command --> go back to sleep and go through another standby mode loop
-    //could potentially trigger due to failed command read
-    SerialUSB.println("Doing no command... going to sleep");
-    break;
+      //could potentially trigger due to failed command read
+      SerialUSB.println("Doing no command... going to sleep");
+      break;
   }
 }
+
+int getSensorMetadata(char* buf, char* metadata){
+  int i = 0;
+  char currChar = 1;
+  SerialUSB.print("Sensor Metadata: ");
+  while(currChar != '\0'){
+    currChar = buf[i+COMMAND_WIDTH];
+    metadata[i] = currChar;
+    SerialUSB.print(metadata[i]);
+    i++;
+  }
+  SerialUSB.println();
+  SerialUSB.print("Metadata length: ");
+  SerialUSB.println(i-2);
+  return (i-2);
+}
+
 
 #endif
--- KickSat-2/libraries/uplink/uplink.ino
+++ KickSat-2/libraries/uplink/uplink.ino
@@ -1,72 +1,76 @@
-#include <RTCCounter.h>
+#include "uplink.h"
 
-#define LISTENINGDURATION 15000 // milliseconds
 
 void setup() {
   SerialUSB.begin(115200);
   while(!SerialUSB);
   delay(5000);
+  configFile.init();
+  configFile.setStandby();
 }
 
 void loop() {
   output_menu();
-  listenForUplink();
+  char buf[50];
+  uint8_t command = listenForUplink(buf, LISTENINGDURATION);
+  SerialUSB.print("Command Index: ");
+  SerialUSB.println(command);
+  SerialUSB.print("Metadata: ");
+  SerialUSB.print(buf[COMMAND_WIDTH]);
   SerialUSB.println();
-  delay(2000);
+  processUplink(buf, command);
+  checkConfigStatus();
+  SerialUSB.println();
+  delay(5000);
 }
 
-///////////////////////
-// Listen for uplink //
-//////////////////////////////////////////////////////////////////////////////
-uint8_t listenForUplink() {
-  SerialUSB.println("Listening for uplink...");
-  timeout.start(LISTENINGDURATION);
-  while(1) { // Wait for uplink, retreive from buffer
-    if (SerialUSB.available()) { // TODO: change if radio is available, not serial
-      uint8_t command = parseUplink();
-      if (command > 0) {
-        return command;
-      }
-    }
-    if (timeout.triggered()) { // Checks time for timeout
-      SerialUSB.println("Uplink Timeout");
-      return 0;
-    }
-  }
-}
 
-uint8_t parseUplink() {
-  ////////////////////////////////////////////////////////
-  // TODO: change from Serial.read to radio read
-  uint8_t command = SerialUSB.read(); // Reads the available char (ASCII)
-  command = command - 48; // Convert char to int (ASCII)
-  ////////////////////////////////////////////////////////
-
-  ////////////////////////////////////////////////////////
-  // TODO: Incorporate real checksum check here for input
-  if (command < 1 || command > 8) {
-    SerialUSB.println("NACK");
-    return 0;
-  } else {
-    SerialUSB.println("ACK");
-    return command;
-  }
-  ////////////////////////////////////////////////////////
-}
-
-///////////////////////
-// Listen for uplink //
-//////////////////////////////////////////////////////////////////////////////
+/////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void output_menu() { // TODO: this is an artifact of serial, it gets replaced by the ground station receiving a beacon
   // Listen for Uplink:
   SerialUSB.println("Choose a command from the following menu: ");
-  SerialUSB.println("1) Send Sensor Data");
-  SerialUSB.println("2) Downlink last 10 Logs");
-  SerialUSB.println("3) Rewrite the sensor config files");
-  SerialUSB.println("4) Reflash the motherboard's code from MRAM");
-  SerialUSB.println("5) Send Mission Config Files");
-  SerialUSB.println("6) Enter Arming Mode");
-  SerialUSB.println("7) Enter End of Life Mode");
-  SerialUSB.println("8) No command");
+  SerialUSB.println("dog) Downlink Data");
+  SerialUSB.println("cat) Set Arming Mode");
+  SerialUSB.println("pig) Reflash Sensor Config");
+  SerialUSB.println("owl) Do Nothing");
+  SerialUSB.println("brn) Burn Wires");
+
+  
   SerialUSB.print("Choose a command: ");
 }
+
+void checkConfigStatus(){
+  SerialUSB.println();
+  SerialUSB.print("Status: ");
+  SerialUSB.println((char)configFile.getStatus());
+  SerialUSB.print("AB1 deployed: ");
+  SerialUSB.print(configFile.getAB1status());
+  SerialUSB.print(", AB2 deployed: ");
+  SerialUSB.print(configFile.getAB2status());
+  SerialUSB.print(", DB1 deployed: ");
+  SerialUSB.print(configFile.getDB1status());
+  SerialUSB.print(", DB2 deployed: ");
+  SerialUSB.print(configFile.getDB2status());
+  SerialUSB.print(", DB3 deployed: ");
+  SerialUSB.println(configFile.getDB3status());
+  SerialUSB.print("DB1 Flag: ");
+  SerialUSB.print(configFile.getDB1FlagStatus());
+  SerialUSB.print(", DB2 Flag: ");
+  SerialUSB.print(configFile.getDB2FlagStatus());
+  SerialUSB.print(", DB3 Flag: ");
+  SerialUSB.println(configFile.getDB3FlagStatus());
+  SerialUSB.print("Hold mode: ");
+  SerialUSB.print(configFile.getHoldstatus());
+  SerialUSB.print(", Standby mode: ");
+  SerialUSB.print(configFile.getStandbyStatus());
+  SerialUSB.print(", Armed mode: ");
+  SerialUSB.print(configFile.getArmedStatus());
+  SerialUSB.print(", Deployed mode: ");
+  SerialUSB.println(configFile.getDeployedStatus());
+  SerialUSB.print("Antenna Timer: ");
+  SerialUSB.println(configFile.checkAntennaTimer());
+  SerialUSB.println();
+  delay(500);
+}
+
