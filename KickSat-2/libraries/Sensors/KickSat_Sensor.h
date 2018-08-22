94e7cd08957afaa712ea0daffb0ddf4e0f1f4ce3
Connor Settle <csettle@stanford.edu>
2018-08-21 17:11:58 -0700

    fixed various config bugs, now working
    NOTE: since I was developing on the Metro Mini, which has limited  memory,  I commented out a few really memory-hungry functions. These functions still exist  in the program, they just need to be uncommented.

--- KickSat-2/libraries/Sensors/KickSat_Sensor.h
+++ KickSat-2/libraries/Sensors/KickSat_Sensor.h
@@ -3,8 +3,6 @@
 #ifndef KICKSAT_SENSOR_H_
 #define KICKSAT_SENSOR_H_
 
-#include <SD.h>
-
 class KickSat_Sensor
 {
 
@@ -13,27 +11,35 @@ class KickSat_Sensor
     void operate(byte* dataOut);
     void parseMessage(String msg, String arg[]);
     void handleCommand(String cmd, byte* buf, int* index);
-    String getCommand(String data, char separator, int index);
-    bool validateConfigFiles();
-    void rewriteConfig(byte* buf, int len);
     void burstWriteRegs(byte* data, uint8_t len);
     void startADC();
     void stopADC();
     void resetADC();
     void shutdownADC();
     void wakeADC();
-    void regReadout();
+    //void regReadout();
+    void mosfetV(byte pinNum);
+    bool validateConfigFiles();
+    //void readTemp();
+    String getCommand(String data, char separator, int index);
+    float dataConvert( byte a, byte b, byte c);
 
   private:
     int _ADCchipSelect;
     int _ADCreset;
     int _SDchipSelect;
     String _configFileName;
-    File _configFile;
-
+    byte val1, val2, val3;
     byte data_filter_chop_reg = 0x17;
     byte vbias_reg = 0x0;
     byte gain_pga_reg = 0x8;
+
+
+    const float refV = 2.5; //reference voltage for the ADC. Usually use internal 2.5V by setting the registers below
+    const float pgaGain = 1;
+    const float FSR = (refV*2)/pgaGain;
+    const float LSBsize = FSR/pow(2,24);
+    bool showHex = true;
 };
 
 #endif /* KICKSAT_SENSOR_H_ */
