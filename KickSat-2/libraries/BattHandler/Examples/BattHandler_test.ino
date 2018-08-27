b1261505e736b30a02330b7df7c1cdc56da7b986
Andrea Ramirez <ityramirez@gmail.com>
2018-08-27 11:52:05 -0700

    update example

--- KickSat-2/libraries/BattHandler/Examples/BattHandler_test.ino
+++ KickSat-2/libraries/BattHandler/Examples/BattHandler_test.ino
@@ -1,5 +1,8 @@
-#include "SAMD_AnalogCorrection.h"
-#include <ATSAMD21_ADC.h>
+/*
+ * BattHandler test - Tests all functions in the BattHandler library
+ */
+
+
 #include <BattHandler.h>
 
 BattHandle power;
@@ -10,35 +13,29 @@ void setup() {
   delay(2000);
   BattHandle();
   delay(200);
-  
 }
 
 void loop() {
-//  analogReadCorrection(2, 2053); 
-  SerialUSB.println("Init");
-  for(int i = 0; i < 20; i++) {
-    float battery = power.readBattVoltage();
-    SerialUSB.print("Battery Voltage:\t");
-    SerialUSB.print(battery,3);
-    SerialUSB.println(" V");
-    delay(500);
-    float battCurr = power.readBattCurrent();
-    SerialUSB.print("Current Draw:\t\t");
-    SerialUSB.print(battCurr);
-    SerialUSB.println(" mA");
-    delay(500);
-    float battChrg = power.readBattChargeCurrent();
-    SerialUSB.print("Charging Current:\t");
-    SerialUSB.print(battChrg);
-    SerialUSB.println(" mA");
-    delay(500);
-//    power.read(buff);
-//    SerialUSB.print(buff[0]);
-//    SerialUSB.print("    ");
-//    SerialUSB.print(buff[1]);
-//    SerialUSB.print("    ");
-//    SerialUSB.println(buff[2]);
-//    delay(500);
-//  }               
-  }
+  float battCurr = power.readBattCurrent();
+  SerialUSB.print("Current Draw:\t\t");
+  SerialUSB.print(battCurr);
+  SerialUSB.println(" mA");
+  delay(500);
+  float battery = power.readBattVoltage();
+  SerialUSB.print("Battery Voltage:\t");
+  SerialUSB.print(battery,3);
+  SerialUSB.println(" V");
+  delay(500);
+  float battChrg = power.readBattChargeCurrent();
+  SerialUSB.print("Charging Current:\t");
+  SerialUSB.print(battChrg);
+  SerialUSB.println(" mA");
+  delay(500);
+  power.read(buff);
+  SerialUSB.print(buff[0]);
+  SerialUSB.print("    ");
+  SerialUSB.print(buff[1]);
+  SerialUSB.print("    ");
+  SerialUSB.println(buff[2]);
+  delay(500);              
 }
