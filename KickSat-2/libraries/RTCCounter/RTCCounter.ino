d0b2bca7b72f431c4c992ec5233e96ecc49cc1e4
Andrea Ramirez <ityramirez@gmail.com>
2018-08-27 14:25:50 -0700

    Revert "Merge branch 'master' of https://github.com/kicksat/FlightSoftware"
    This reverts commit 66a92ab5b72631f26dc35c99079e1353cb6e7041.

---
+++ KickSat-2/libraries/RTCCounter/RTCCounter.ino
@@ -0,0 +1,37 @@
+
+#include "RTCCounter.h"
+
+Counter watchdogTimer; // creates timer object
+Counter beaconTimer; // creates timer object
+
+void watchdog() {
+  SerialUSB.println("Watchdog");
+}
+
+void setup() {
+  if (!SerialUSB) {
+    SerialUSB.begin(115200); // Restart SerialUSB
+    while(!SerialUSB); // Wait for serial USB port to open
+    SerialUSB.println("Serial Initialized");
+    delay(5000); // Provides user with time to open Serial Monitor
+  }
+  watchdogTimer.init(1000,watchdog); // timer delay, milliseconds
+  SerialUSB.println("Watchdog Timer Initialized");
+  beaconTimer.init(10000); // timer delay, milliseconds
+  SerialUSB.println("Beacon Timer Initialized");
+  SerialUSB.println("Setup Complete");
+}
+
+void loop() {
+  if (beaconTimer.check()) { // Checks time for interrupt
+    SerialUSB.println("Beacon things!");
+  }
+  timeout.start(2000); // start a timeout timer delay, milliseconds
+  while(1) {
+    if (timeout.triggered()) { // Checks time for timeout
+      SerialUSB.println("TIMEOUT");
+      break;
+    }
+  }
+  sleepTimer.sleep(); // Go into sleep mode until next interrupt
+}
