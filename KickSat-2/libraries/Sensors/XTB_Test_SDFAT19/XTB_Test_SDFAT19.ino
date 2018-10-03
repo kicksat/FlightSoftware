2a35963ca571babed06aea5c6b6039e6fba7e1bc
Max Holliday <maholli@stanford.edu>
2018-10-03 08:59:58 -0700

    concise sensor data packing and retrival - WORKING ON FEATHER

--- KickSat-2/libraries/Sensors/XTB_Test_SDFAT19/XTB_Test_SDFAT19.ino
+++ KickSat-2/libraries/Sensors/XTB_Test_SDFAT19/XTB_Test_SDFAT19.ino
@@ -19,7 +19,7 @@ void setup() {
   delay(200);
   SPI.begin(); 
   if (SD.begin(SD_CS)) {
-    datafile3 = SD.open("xtb12.dat", FILE_WRITE);
+    datafile3 = SD.open("xtb2.dat", FILE_WRITE);
     Serial.println("SDcard initalized");
   }
 }
@@ -28,21 +28,24 @@ uint8_t cnt=0;
 void loop() {
   float buf1[4];
   kSensor.operate(buf1, "xtb1"); //operate the sensor board and store data in buf  
-  for (uint8_t i=0; i<4; i++){
-    Serial.println(buf1[i],8);
-    datafile3.write((const float *)&buf1[i], sizeof(buf1[i])); //save buf to SD card as bytes (4B/float)
-  }
+  datafile3.write((const uint8_t *)&buf1, sizeof(buf1)); //save buf to SD card as bytes (4B per float)
   datafile3.flush();
-  delay(1000);
-  
-  if (cnt == 4){ //example of reading out the packed data from SD card
-    datafile3.close();
-    datafile3 = SD.open("xtb12.dat", FILE_READ);
-    struct datastore myData;
-    datafile3.read((float *)&myData, sizeof(myData));
-    Serial.println(myData.dat1,4);
-    Serial.println(myData.dat1,8);
-    while(1);
-  }
-  cnt ++;
+  delay(2000);
+  readoutData();
 }
+
+void readoutData(){
+  if (datafile3.isOpen()) {
+    datafile3.close();
+  }
+  datafile3 = SD.open("xtb2.dat", FILE_READ);
+  struct datastore testData;
+  datafile3.read((uint8_t *)&testData, sizeof(testData));
+  Serial.println(testData.dat1,8);
+  Serial.println(testData.dat2,8);
+  Serial.println(testData.dat3,8);
+  Serial.println(testData.dat4,8);
+  datafile3.close();
+  while(1);
+}
+
